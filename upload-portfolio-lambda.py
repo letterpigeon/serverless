a48333f0049fa2b8c0bfbc9608645133e5058023
letterpigeon <ban.luc@gmail.com>
2017-10-16 23:49:55 +0800

    add sns notification

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,16 +9,25 @@ import mimetypes
 
 def handler(event, context):
     s3 = boto3.resource("s3")
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:ap-southeast-1:089181805188:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket("portfolio.culnab.com")
-    build_bucket = s3.Bucket('portfoliobuild.culnab.com')
+    try:
 
-    serverless_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('serverlessbuild.zip', serverless_zip)
+        portfolio_bucket = s3.Bucket("portfolio.culnab.com")
+        build_bucket = s3.Bucket('portfoliobuild.culnab.com')
 
-    with zipfile.ZipFile(serverless_zip) as myzip:
-        for name in myzip.namelist():
-            obj = myzip.open(name)
-            portfolio_bucket.upload_fileobj(obj, name,
-                ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        serverless_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('serverlessbuild.zip', serverless_zip)
+
+        with zipfile.ZipFile(serverless_zip) as myzip:
+            for name in myzip.namelist():
+                obj = myzip.open(name)
+                portfolio_bucket.upload_fileobj(obj, name,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+                portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio deployment failed")
+        raise
