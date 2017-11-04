9b275dea0d27cbe6d05f542812239199743118f3
letterpigeon <ban.luc@gmail.com>
2017-11-04 16:37:35 +0800

    removing unused work example

--- js/main.js
+++ js/main.js
@@ -4,34 +4,13 @@ import ExampleWork from './example-work';
 
 const myWork = [
   {
-    'title': "Work Example 1",
+    'title': "Serverless Website",
     'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "This website is the end product of an ACloudGuru course to build a website to showcase one's work portfolio using serverless technology.  It uses AWS S3, Lambda, CloudFront, Route53 for hosting the website, Github, CodeBuild, Lambda to automate continuous building and deployment, uses ReactJS, Babel, webpack, jest to do modern javascript development, testing, packaing",
     'image': {
-      'desc': "Work Example 1",
-      'src': "images/example1.png",
-      'comment': "Work Example 1"
-    }
-  },
-  {
-    'title': "Work Example 2",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'image': {
-      'desc': "Serverless",
+      'desc': "Serverless Portfolio",
       'src': "images/example2.png",
       'comment': "A Serverless Portfolio"
-    }
-  },
-  {
-    'title': "Work Example",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    'image': {
-      'desc': "Work Example 3",
-      'src': "images/example3.png",
-      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
-             https://www.flickr.com/photos/37287295@N00/2540855181`
     }
   }
 ]
