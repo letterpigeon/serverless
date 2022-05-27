c4e188eac53b29adb703b531aa46d9d722bde650
letterpigeon <ban.luc@gmail.com>
2022-05-27 12:10:06 +0800

    Update main.js
    fixed typo in serverless website modal

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const myWork = [
   {
     'title': "Serverless Website",
     'href': "https://example.com",
-    'desc': "This website is the end product of an ACloudGuru course to build a website to showcase one's work portfolio using serverless technology.  It uses AWS S3, Lambda, CloudFront, Route53 for hosting the website, Github, CodeBuild, Lambda to automate continuous building and deployment, uses ReactJS, Babel, webpack, jest to do modern javascript development, testing, packaing",
+    'desc': "This website is the end product of an ACloudGuru course to build a website to showcase one's work portfolio using serverless technology.  It uses AWS S3, Lambda, CloudFront, Route53 for hosting the website, Github, CodeBuild, Lambda to automate continuous building and deployment, uses ReactJS, Babel, webpack, jest to do modern javascript development, testing, packaging",
     'image': {
       'desc': "Serverless Portfolio",
       'src': "images/example2.png",
